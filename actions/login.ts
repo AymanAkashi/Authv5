cc460eb1830331dae5c3fbb99a73050eaafd3ea8
AymanAkashi <aaggoujj@student.1337.ma>
2024-04-27 22:34:21 +0100

    wip: add auth of login page

--- actions/login.ts
+++ actions/login.ts
@@ -3,13 +3,35 @@
 import * as z from 'zod'
 
 import { LoginSchema } from '@/schemas'
+import { signIn } from '@/auth'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
+import { AuthError } from 'next-auth'
+import { error } from 'console'
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-	const validatedFields = LoginSchema.safeParse(values);
+    const validatedFields = LoginSchema.safeParse(values)
 
-	if (!validatedFields.success) {
-		throw new Error('Invalid fields!');
-	}
+    if (!validatedFields.success) {
+        throw new Error('Invalid fields!')
+    }
 
-	return { success: "Email sent!"};
+    const { email, password } = validatedFields.data
+
+    try {
+        await signIn('credentials', {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.message) {
+                case 'CredentialsSignin':
+                    return { error: 'Invalid credentials!' }
+                default:
+                    return { error: 'An error occurred' }
+            }
+        }
+        throw error
+    }
 }
